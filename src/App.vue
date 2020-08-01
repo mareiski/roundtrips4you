bdd775cd80dbc91a24ff984d9267882c7790265e
mareiski <marco.reisinger@t-online.de>
2020-08-01 15:42:47 +0200

    changed browser support

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="q-app">
     <router-view />
+    <span
+      id="notSupportedBrowserSpan"
+      style="display:none;"
+    >Dein Browser wird leider nicht unterstützt. Bitte füre ein Update durch.</span>
   </div>
 </template>
 
 <script>
+const supportedBrowsers = require('../supportedBrowsers')
+
+if (!supportedBrowsers.test(navigator.userAgent)) {
+  alert('Dein browser wird leider nicht nicht unterstützt.')
+}
 export default {
   name: 'App'
 }
