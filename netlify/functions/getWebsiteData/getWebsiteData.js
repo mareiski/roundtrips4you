227839df99668b2cdd54bc4706803b2cab64fc6a
mareiski <marco.reisinger@t-online.de>
2021-05-08 18:13:05 +0200

    next attempt

--- netlify/functions/getWebsiteData/getWebsiteData.js
+++ netlify/functions/getWebsiteData/getWebsiteData.js
@@ -3,16 +3,22 @@ const axios = require('axios')
 
 exports.handler = async (event) => {
   let url = event.queryStringParameters.url || 'https://www.booking.com/hotel/fr/elyseesunion.de.html'
+
+  let response = null
+
   await axios.get(url)
     .then(page => {
       console.log(page.data)
-      return {
-        statusCode: 200,
-        body: page.data
-      }
+      response = page.data
     })
     .catch(error => {
       console.log(error)
       return { statusCode: 500, body: error }
     })
+
+  console.log(response)
+  return {
+    statusCode: response ? 200 else 500,
+    body: response || 'failed'
+  }
 }
