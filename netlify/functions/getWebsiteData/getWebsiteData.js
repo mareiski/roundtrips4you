8b411f2cc349d9e69f27c36cb6ed9f0208ea5bc7
mareiski <marco.reisinger@t-online.de>
2021-05-08 17:57:11 +0200

    changed function

--- netlify/functions/getWebsiteData/getWebsiteData.js
+++ netlify/functions/getWebsiteData/getWebsiteData.js
@@ -3,16 +3,12 @@ const axios = require('axios')
 
 exports.handler = async (event) => {
   let url = event.queryStringParameters.url || 'https://www.booking.com/hotel/fr/elyseesunion.de.html'
-  console.log(url)
   await axios.get(url)
     .then(page => {
       console.log(page.data)
       return {
         statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({ message: page.data })
+        body: page.data
       }
     })
     .catch(error => {
